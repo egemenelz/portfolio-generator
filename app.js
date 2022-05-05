0d13ba641abc7e1784069823ebfbca0dd9614c26
egemenelz <egemenelz@gmail.com>
2022-05-05 00:27:58 -0400

    update

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?'
+//     }
+//   ])
+//   .then(answers => console.log(answers));
 
-const name = profileDataArgs[0];
-const gitHub = profileDataArgs[1];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+};
+
+promptUser().then(answers => console.log(answers));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is how we do manually, 
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const name = profileDataArgs[0];
+// const gitHub = profileDataArgs[1];
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, gitHub);
+
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 // const generatePage = () => 'Name : Egemen, GitHub : egemenelz';
 //  =====================
@@ -17,14 +74,4 @@ const gitHub = profileDataArgs[1];
 //   `;
 // }
 
-
-
-
-
 //console.log(generatePage(name, gitHub));
-
-fs.writeFile('index.html', generatePage(name, gitHub), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
